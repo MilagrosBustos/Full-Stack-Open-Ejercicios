54484ae6188258a1faca5b6d3dc9bf8f20e77f1a
MilagrosBustos <73721555+MilagrosBustos@users.noreply.github.com>
2026-03-10 18:25:51 -0300

    Add files via upload

---
+++ Parte 1 - Introducción a React/informacion-curso/src/App.jsx
@@ -0,0 +1,60 @@
+const Header = (props) => {
+  return(
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Part1 = (props) => {
+  return(
+    <div>
+      <p>{props.part}: {props.exercises}</p>
+    </div>
+  )
+}
+
+const Part2 = (props) => {
+  return(
+    <div>
+      <p>{props.part}: {props.exercises}</p>
+    </div>
+  )
+}
+
+const Part3 = (props) => {
+  return(
+    <div>
+      <p>{props.part}: {props.exercises}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return(
+    <div>
+      <p>Number of exercises: {props.exercises1 + props.exercises2 + props.exercises3}</p>  
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {name:'Fundamentals of React', exercises:10},
+    {name:'Using props to pass data', exercises:7},
+    {name:'State of a component', exercises:14},
+  ]
+
+  return(
+    <>
+      <Header course={course} />
+      <Part1 part={parts[0].name} exercises={parts[0].exercises}/>
+      <Part2 part={parts[1].name} exercises={parts[1].exercises}/>
+      <Part3 part={parts[2].name} exercises={parts[2].exercises}/>
+      <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises}/>
+    </>
+  )
+}
+
+export default App
